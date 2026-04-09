97d2293fffbbdb03d601099b02a156cdc871f62a
Shruti Kiran Totla <shruti.totla@HYD.RECYKAL.COM>
2026-04-09 18:09:22 +0530

    Fix import casing for Linux deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
   import { BrowserRouter, Routes, Route } from 'react-router-dom'
-  import Login from './pages/Login'
-  import Home from './pages/Home'
+  import Login from './pages/login'
+  import Home from './pages/home'
 
   function App() {
     return (
